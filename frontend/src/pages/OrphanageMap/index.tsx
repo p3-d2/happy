fddbdd855ae3aec0180b99d009e17f5cdecb18be
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-16 19:23:38 -0300

    fixing comment

--- frontend/src/pages/OrphanageMap/index.tsx
+++ frontend/src/pages/OrphanageMap/index.tsx
@@ -44,7 +44,7 @@ const OrphanageMap: React.FC = () => {
       >
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-          {/* don't copy my credential */}
+          /* don't copy my credential */
         />
       </Map>
 
