a3bc637b8d9fbffd7b16cd7d597a95889d9869e1
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-16 18:19:10 -0300

    stylization the Home page

--- frontend/src/pages/Landing/index.tsx
+++ frontend/src/pages/Landing/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import useTheme from '../../utils/useTheme'
 
-import { Container } from './styles'
+import {
+  Container,
+  Wrapper,
+  Imagem,
+  Main,
+  Title,
+  Details,
+  Location,
+  Strong,
+  Span,
+  Link,
+  ArrowIcon
+} from './styles'
+
+import Logo from '../../assets/Logo.svg'
 
 const Landing: React.FC = () => {
+  const { colors } = useTheme()
+
   return (
     <Container>
-      <h1>Landing</h1>
+      <Wrapper>
+        <Imagem src={Logo} alt="Happy" />
+
+        <Main>
+          <Title>Leve felicidade para o mundo</Title>
+          <Details>Visite orfanatos e mude o dia de muitas crianças</Details>
+        </Main>
+
+        <Location>
+          <Strong>Rio de Janeiro</Strong>
+          <Span>Rio de Janeiro</Span>
+        </Location>
+
+        <Link to="/app">
+          <ArrowIcon size={26} color={colors.shadow} />
+        </Link>
+      </Wrapper>
     </Container>
   )
 }
--- frontend/src/pages/Landing/styles.ts
+++ frontend/src/pages/Landing/styles.ts
@@ -1,3 +1,95 @@
 import styled from 'styled-components'
+import { Link as ReactRouterDomLink } from 'react-router-dom'
+import { FiArrowRight } from '../../styles/icons'
 
-export const Container = styled.div``
+import Landing from '../../assets/landing.svg'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: ${props =>
+    `linear-gradient(329.54deg, ${props.theme.colors.primary}, ${props.theme.colors.secondary})`};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Wrapper = styled.div`
+  position: relative;
+
+  max-width: 1100px;
+  width: 100%;
+
+  max-height: 600px;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  background: url(${Landing}) no-repeat 80% center;
+  background-size: contain;
+`
+
+export const Imagem = styled.img``
+
+export const Main = styled.main`
+  max-width: 350px;
+`
+
+export const Title = styled.h1`
+  font-weight: ${props => props.theme.fontWeights.extraBold};
+  font-size: ${props => props.theme.fontSizes.big};
+  line-height: ${props => props.theme.lineHeights.big};
+`
+
+export const Details = styled.p`
+  margin-top: 40px;
+  font-size: ${props => props.theme.fontSizes.normal};
+  line-height: ${props => props.theme.lineHeights.normal};
+`
+
+export const Location = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+
+  font-size: ${props => props.theme.fontSizes.normal};
+  line-height: ${props => props.theme.lineHeights.normal};
+
+  display: flex;
+  flex-direction: column;
+
+  text-align: right;
+`
+
+export const Strong = styled.strong`
+  font-weight: ${props => props.theme.fontWeights.bold};
+`
+
+export const Span = styled.span``
+
+export const Link = styled(ReactRouterDomLink)`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+
+  width: 80px;
+  height: 80px;
+  background: ${props => props.theme.colors.quaternary};
+  border-radius: 30px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  transition: background-color 500ms;
+
+  &:hover {
+    background: ${props => props.theme.colors.tertiary};
+  }
+`
+
+export const ArrowIcon = styled(FiArrowRight)``
