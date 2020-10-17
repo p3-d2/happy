78b4db86441be0c11c4e80f110a482542e1c34e3
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-17 11:41:19 -0300

    redeployment to the use of controllers

---
+++ backend/src/controllers/OrphanageControllers.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from '../models/Orphanage'
+
+export default {
+  async index(req: Request, res: Response): Promise<any> {
+    const orphanageRepository = getRepository(Orphanage)
+
+    const orphanages = await orphanageRepository.find()
+
+    return res.status(200).json(orphanages)
+  },
+
+  async show(req: Request, res: Response): Promise<any> {
+    const { id } = req.params
+
+    const orphanageRepository = getRepository(Orphanage)
+
+    const orphanage = await orphanageRepository.findOneOrFail(id)
+
+    return res.status(200).json(orphanage)
+  },
+
+  async create(req: Request, res: Response): Promise<any> {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends
+    } = req.body
+
+    const orphanageRepository = getRepository(Orphanage)
+
+    const requestImages = req.files as Express.Multer.File[]
+
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
+    const orphanage = orphanageRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      images
+    })
+
+    await orphanageRepository.save(orphanage)
+
+    return res.status(201).json(orphanage)
+  }
+}
