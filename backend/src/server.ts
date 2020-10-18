c55b546ad12c1ae0f280b38bdf7e63d5cdcb1776
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-18 16:30:28 -0300

    adding authentication in the api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,22 @@
 import express from 'express'
+import cors from 'cors'
 import path from 'path'
 import 'express-async-errors'
-
-import './database/connection'
 
 import routes from './routes'
 import errorHandler from './errors/handler'
 
+import './database/connection'
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler)
 
-app.listen(3333)
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')))
+
+app.listen(3333, () => {
+  console.log('Server started!')
+})
