a5dcd9ee947106f7648d19c2d6cf2187bcc7b81d
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-19 08:39:41 -0300

    stylizing details page of an orphanage

--- frontend/src/pages/Orphanage/index.tsx
+++ frontend/src/pages/Orphanage/index.tsx
@@ -1,13 +1,150 @@
 import React from 'react'
+import { Map, TileLayer, Marker } from 'react-leaflet'
+import useTheme from '../../utils/useTheme'
 
-import { Container } from './styles'
+import {
+  Container,
+  Main,
+  OrphanageDetails,
+  ImgPrincipal,
+  Imgs,
+  ButtonImg,
+  Img,
+  OrphangeDetailsContent,
+  Title,
+  Details,
+  MapContainer,
+  Footer,
+  A,
+  Hr,
+  SubTitle,
+  OpenDetails,
+  Hour,
+  ClockIcon,
+  Br,
+  OpenOnWeekends,
+  InfoIcon,
+  ContactButton,
+  WhatsappIcon
+} from './styles'
 
 import SideBar from '../../components/SideBar'
 
+import mapIcon from '../../utils/mapIcon'
+
 const Orphanage: React.FC = () => {
+  const { colors } = useTheme()
+
   return (
     <Container>
       <SideBar />
+
+      <Main>
+        <OrphanageDetails>
+          <ImgPrincipal
+            src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+            alt="Lar das meninas"
+          />
+
+          <Imgs className="images">
+            <ButtonImg className="active" type="button">
+              <Img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </ButtonImg>
+            <ButtonImg type="button">
+              <Img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </ButtonImg>
+            <ButtonImg type="button">
+              <Img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </ButtonImg>
+            <ButtonImg type="button">
+              <Img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </ButtonImg>
+            <ButtonImg type="button">
+              <Img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </ButtonImg>
+            <ButtonImg type="button">
+              <Img
+                src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg"
+                alt="Lar das meninas"
+              />
+            </ButtonImg>
+          </Imgs>
+
+          <OrphangeDetailsContent>
+            <Title>Lar das meninas</Title>
+            <Details>
+              Presta assistência a crianças de 06 a 15 anos que se encontre em
+              situação de risco e/ou vulnerabilidade social.
+            </Details>
+
+            <MapContainer>
+              <Map
+                center={[-27.2092052, -49.6401092]}
+                zoom={16}
+                style={{ width: '100%', height: 280 }}
+                dragging={false}
+                touchZoom={false}
+                zoomControl={false}
+                scrollWheelZoom={false}
+                doubleClickZoom={false}
+              >
+                <TileLayer
+                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+                <Marker
+                  interactive={false}
+                  icon={mapIcon}
+                  position={[-27.2092052, -49.6401092]}
+                />
+              </Map>
+
+              <Footer>
+                <A href="">Ver rotas no Google Maps</A>
+              </Footer>
+            </MapContainer>
+
+            <Hr />
+
+            <SubTitle>Instruções para visita</SubTitle>
+            <Details>
+              Venha como se sentir mais à vontade e traga muito amor para dar.
+            </Details>
+
+            <OpenDetails>
+              <Hour>
+                <ClockIcon size={32} color={colors.sideBarGradientPrimary} />
+                Segunda à Sexta <Br />
+                8h às 18h
+              </Hour>
+              <OpenOnWeekends>
+                <InfoIcon size={32} color={colors.infoIcon} />
+                Atendemos <Br />
+                fim de semana
+              </OpenOnWeekends>
+            </OpenDetails>
+
+            <ContactButton type="button">
+              <WhatsappIcon size={20} color={colors.white} />
+              Entrar em contato
+            </ContactButton>
+          </OrphangeDetailsContent>
+        </OrphanageDetails>
+      </Main>
     </Container>
   )
 }
--- frontend/src/pages/Orphanage/styles.ts
+++ frontend/src/pages/Orphanage/styles.ts
@@ -1,3 +1,185 @@
 import styled from 'styled-components'
+import { FiClock, FiInfo, FaWhatsapp } from '../../styles/icons'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  min-height: 100vh;
+`
+
+export const Main = styled.main`
+  flex: 1;
+`
+
+export const OrphanageDetails = styled.div`
+  width: 700px;
+  margin: 64px auto;
+
+  background: ${props => props.theme.colors.white};
+  border: 1px solid ${props => props.theme.colors.borderForm};
+  border-radius: 20px;
+
+  overflow: hidden;
+`
+
+export const ImgPrincipal = styled.img`
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+`
+
+export const Imgs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  column-gap: 16px;
+
+  margin: 16px 40px 0;
+`
+
+export const ButtonImg = styled.button`
+  border: 0;
+  height: 88px;
+  background: none;
+  cursor: pointer;
+  border-radius: 20px;
+  overflow: hidden;
+  outline: none;
+
+  opacity: 0.6;
+
+  &.active {
+    opacity: 1;
+  }
+`
+
+export const Img = styled.img`
+  width: 100%;
+  height: 88px;
+  object-fit: cover;
+`
+
+export const OrphangeDetailsContent = styled.div`
+  padding: 80px;
+`
+
+export const Title = styled.h1`
+  color: ${props => props.theme.colors.title};
+  font-size: ${props => props.theme.fontSizes.big};
+  line-height: ${props => props.theme.lineHeights.big};
+  margin-bottom: 8px;
+`
+
+export const Details = styled.p`
+  line-height: ${props => props.theme.lineHeights.normal};
+  color: ${props => props.theme.colors.legendColor};
+  margin-top: 24px;
+`
+
+export const MapContainer = styled.div`
+  margin-top: 64px;
+  background: ${props => props.theme.colors.backgroundContainer};
+  border: 1px solid ${props => props.theme.colors.borderContainer};
+  border-radius: 20px;
+
+  .leaflet-container {
+    border-bottom: 1px solid ${props => props.theme.colors.leafletContainer};
+    border-radius: 20px;
+  }
+`
+
+export const Footer = styled.footer`
+  padding: 20px 0;
+  text-align: center;
+`
+
+export const A = styled.a`
+  line-height: ${props => props.theme.lineHeights.small};
+  color: ${props => props.theme.colors.colorPopup};
+`
+
+export const Hr = styled.hr`
+  width: 100%;
+  height: 1px;
+  border: 0;
+  background: ${props => props.theme.colors.hrColor};
+  margin: 64px 0;
+`
+
+export const SubTitle = styled.h2`
+  font-size: ${props => props.theme.fontSizes.legend};
+  line-height: ${props => props.theme.lineHeights.big};
+  color: ${props => props.theme.colors.title};
+`
+
+export const OpenDetails = styled.div`
+  margin-top: 24px;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 20px;
+
+  div {
+    padding: 32px 24px;
+    border-radius: 20px;
+    line-height: 28px;
+
+    svg {
+      display: block;
+      margin-bottom: 20px;
+    }
+  }
+`
+
+export const Hour = styled.div`
+  background: linear-gradient(
+    149.97deg,
+    ${props => props.theme.colors.backgroundContainer} 8.13%,
+    ${props => props.theme.colors.white} 92.67%
+  );
+  border: 1px solid ${props => props.theme.colors.borderContainer};
+  color: ${props => props.theme.colors.legendColor};
+`
+
+export const ClockIcon = styled(FiClock)``
+
+export const Br = styled.br``
+
+export const OpenOnWeekends = styled.div`
+  background: linear-gradient(
+    154.16deg,
+    ${props => props.theme.colors.buttonActiveBackground} 7.85%,
+    ${props => props.theme.colors.white} 91.03%
+  );
+  border: 1px solid ${props => props.theme.colors.buttonActiveBorder};
+  color: ${props => props.theme.colors.buttonActiveColor};
+`
+
+export const InfoIcon = styled(FiInfo)``
+
+export const ContactButton = styled.button`
+  margin-top: 64px;
+
+  width: 100%;
+  height: 64px;
+  border: 0;
+  cursor: pointer;
+  background: ${props => props.theme.colors.buttonConfirmBackground};
+  border-radius: 20px;
+  color: ${props => props.theme.colors.white};
+  font-weight: 800;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: ${props => props.theme.colors.buttonConfirmHover};
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`
+
+export const WhatsappIcon = styled(FaWhatsapp)``
