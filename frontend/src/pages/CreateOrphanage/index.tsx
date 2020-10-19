1ceaaf9d22309ef72f9f3e6f595272058373fbc7
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-19 07:51:26 -0300

    stylizing the orphanage creation page

--- frontend/src/pages/CreateOrphanage/index.tsx
+++ frontend/src/pages/CreateOrphanage/index.tsx
@@ -1,13 +1,106 @@
 import React from 'react'
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
-import { Container } from './styles'
+import {
+  Container,
+  Main,
+  Form,
+  Fieldset,
+  Legend,
+  InputBlock,
+  Label,
+  Input,
+  Span,
+  TextArea,
+  UploadedImages,
+  NewImage,
+  PlusIcon,
+  ButtonSelectDiv,
+  ButtonSelect,
+  ConfirmButton
+} from './styles'
 
 import SideBar from '../../components/SideBar'
+
+import mapIcon from '../../utils/mapIcon'
 
 const CreateOrphanage: React.FC = () => {
   return (
     <Container>
       <SideBar />
+
+      <Main>
+        <Form>
+          <Fieldset>
+            <Legend>Dados</Legend>
+
+            <Map
+              center={[-27.2092052, -49.6401092]}
+              style={{ width: '100%', height: 280 }}
+              zoom={15}
+            >
+              <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+              />
+
+              <Marker
+                interactive={false}
+                icon={mapIcon}
+                position={[-27.2092052, -49.6401092]}
+              />
+            </Map>
+
+            <InputBlock>
+              <Label htmlFor="namme">Nome</Label>
+              <Input id="name" />
+            </InputBlock>
+
+            <InputBlock>
+              <Label htmlFor="about">
+                Sobre <Span>Máximo de 300 caracteres</Span>
+              </Label>
+              <TextArea id="name" maxLength={300} />
+            </InputBlock>
+
+            <InputBlock>
+              <Label htmlFor="images">Fotos</Label>
+
+              <UploadedImages></UploadedImages>
+
+              <NewImage>
+                <PlusIcon size={24} color="#15b6d6" />
+              </NewImage>
+            </InputBlock>
+          </Fieldset>
+
+          <Fieldset>
+            <Legend>Visitação</Legend>
+
+            <InputBlock>
+              <Label htmlFor="instructions">Instruções</Label>
+              <TextArea id="instructions" />
+            </InputBlock>
+
+            <InputBlock>
+              <Label htmlFor="opening_hours">Nome</Label>
+              <Input id="opening_hours" />
+            </InputBlock>
+
+            <InputBlock>
+              <Label htmlFor="open_on_weekends">Atende fim de semana</Label>
+
+              <ButtonSelectDiv className="button-select">
+                <ButtonSelect type="button" className="active">
+                  Sim
+                </ButtonSelect>
+                <ButtonSelect type="button">Não</ButtonSelect>
+              </ButtonSelectDiv>
+            </InputBlock>
+          </Fieldset>
+
+          <ConfirmButton type="submit">Confirmar</ConfirmButton>
+        </Form>
+      </Main>
     </Container>
   )
 }
--- frontend/src/pages/CreateOrphanage/styles.ts
+++ frontend/src/pages/CreateOrphanage/styles.ts
@@ -1,3 +1,159 @@
 import styled from 'styled-components'
+import { FiPlus } from '../../styles/icons'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+`
+
+export const Main = styled.main`
+  flex: 1;
+`
+
+export const Form = styled.form`
+  width: 700px;
+  margin: 64px auto;
+
+  background: ${props => props.theme.colors.white};
+  border: 1px solid ${props => props.theme.colors.borderForm};
+  border-radius: 20px;
+
+  padding: 64px 80px;
+
+  overflow: hidden;
+
+  fieldset + fieldset {
+    margin-top: 80px;
+  }
+
+  div + div {
+    margin-top: 24px;
+  }
+`
+
+export const Fieldset = styled.fieldset`
+  border: 0;
+`
+
+export const Legend = styled.legend`
+  width: 100%;
+
+  font-size: ${props => props.theme.fontSizes.legend};
+  line-height: ${props => props.theme.lineHeights.normal};
+  color: ${props => props.theme.colors.legendColor};
+  font-weight: ${props => props.theme.fontWeights.bold};
+
+  border-bottom: 1px solid ${props => props.theme.colors.borderForm};
+  margin-bottom: 40px;
+  padding-bottom: 24px;
+`
+
+export const InputBlock = styled.div``
+
+export const Label = styled.label`
+  display: flex;
+  color: ${props => props.theme.colors.labelColor};
+  margin-bottom: 8px;
+  line-height: ${props => props.theme.lineHeights.small};
+`
+
+export const Span = styled.span`
+  font-size: ${props => props.theme.fontSizes.tooSmall};
+  color: ${props => props.theme.colors.labelColor};
+  margin-left: 24px;
+  line-height: ${props => props.theme.lineHeights.small};
+`
+
+export const Input = styled.input`
+  width: 100%;
+  height: 64px;
+  padding: 0 16px;
+
+  background: ${props => props.theme.colors.backgroundInput};
+  border: 1px solid ${props => props.theme.colors.borderForm};
+  border-radius: 20px;
+  outline: none;
+  color: ${props => props.theme.colors.legendColor};
+`
+
+export const TextArea = styled.textarea`
+  width: 100%;
+  min-height: 120px;
+  max-height: 240px;
+  resize: vertical;
+  padding: 16px;
+  line-height: 28px;
+
+  background: ${props => props.theme.colors.backgroundInput};
+  border: 1px solid ${props => props.theme.colors.borderForm};
+  border-radius: 20px;
+  outline: none;
+  color: ${props => props.theme.colors.legendColor};
+`
+
+export const UploadedImages = styled.div``
+
+export const NewImage = styled.button`
+  width: 100%;
+  height: 64px;
+  background: ${props => props.theme.colors.backgroundInput};
+  border: 1px dashed ${props => props.theme.colors.borderImage};
+  border-radius: 20px;
+  cursor: pointer;
+`
+
+export const PlusIcon = styled(FiPlus)``
+
+export const ButtonSelectDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`
+
+export const ButtonSelect = styled.button`
+  height: 64px;
+  background: ${props => props.theme.colors.backgroundInput};
+  border: 1px solid ${props => props.theme.colors.borderForm};
+  color: ${props => props.theme.colors.legendColor};
+  cursor: pointer;
+
+  &:first-child {
+    border-radius: 20px 0px 0px 20px;
+  }
+
+  &:last-child {
+    border-radius: 0 20px 20px 0;
+    border-left: 0;
+  }
+
+  &.active {
+    background: ${props => props.theme.colors.buttonActiveBackground};
+    border: 1px solid ${props => props.theme.colors.buttonActiveBorder};
+    color: ${props => props.theme.colors.buttonActiveColor};
+  }
+`
+
+export const ConfirmButton = styled.button`
+  margin-top: 64px;
+
+  width: 100%;
+  height: 64px;
+  border: 0;
+  cursor: pointer;
+  background: ${props => props.theme.colors.buttonConfirmBackground};
+  border-radius: 20px;
+  color: ${props => props.theme.colors.white};
+  font-weight: 800;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: ${props => props.theme.colors.buttonConfirmHover};
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`
