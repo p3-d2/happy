31ae074cd161264600bc64b8749ce2bb59502a72
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-19 06:42:16 -0300

    creating new pages

---
+++ frontend/src/pages/CreateOrphanage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const CreateOrphanage: React.FC = () => {
+  return (
+    <Container>
+      <h1>CreateOrphanage</h1>
+    </Container>
+  )
+}
+
+export default CreateOrphanage
---
+++ frontend/src/pages/Orphanage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Orphanage: React.FC = () => {
+  return (
+    <Container>
+      <h1>Orphanage</h1>
+    </Container>
+  )
+}
+
+export default Orphanage
