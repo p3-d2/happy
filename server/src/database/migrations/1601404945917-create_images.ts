bd0091b51a1d6da61602e242dbf5b609af5234b2
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-20 10:41:50 -0300

    forcing a code style

--- server/src/database/migrations/1601404945917-create_images.ts
+++ server/src/database/migrations/1601404945917-create_images.ts
@@ -1,38 +1,40 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
 export class createImages1601404945917 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(new Table({
-      name: 'images',
-      columns: [
-        {
-          name: 'id',
-          type: 'integer',
-          unsigned: true,
-          isPrimary: true,
-          isGenerated: true,
-          generationStrategy: 'increment',
-        },
-        {
-          name: 'path',
-          type: 'varchar'
-        },
-        {
-          name: 'orphanage_id',
-          type: 'integer',
-        }
-      ],
-      foreignKeys: [
-        {
-          name: 'ImageOrphanage',
-          columnNames: ['orphanage_id'],
-          referencedColumnNames: ['id'],
-          referencedTableName: 'orphanages',
-          onUpdate: 'CASCADE',
-          onDelete: 'CASCADE'
-        }
-      ]
-    }))
+    await queryRunner.createTable(
+      new Table({
+        name: 'images',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'path',
+            type: 'varchar'
+          },
+          {
+            name: 'orphanage_id',
+            type: 'integer'
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'ImageOrphanage',
+            columnNames: ['orphanage_id'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'orphanages',
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+          }
+        ]
+      })
+    )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
--- server/src/errors/handler.ts
+++ server/src/errors/handler.ts
@@ -1,24 +1,24 @@
-import { ErrorRequestHandler } from "express";
-import { ValidationError } from "yup";
+import { ErrorRequestHandler } from 'express'
+import { ValidationError } from 'yup'
 
 interface ValidationErrors {
-  [key: string]: string[];
+  [key: string]: string[]
 }
 
-const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+const errorHandler: ErrorRequestHandler = (error, request, response) => {
   if (error instanceof ValidationError) {
-    let errors: ValidationErrors = {};
+    const errors: ValidationErrors = {}
 
     error.inner.forEach(err => {
-      errors[err.path] = err.errors;
-    });
+      errors[err.path] = err.errors
+    })
 
-    return response.status(400).json({ message: 'Validation fails.', errors });
+    return response.status(400).json({ message: 'Validation fails.', errors })
   }
 
-  console.error(error);
+  console.error(error)
 
-  return response.status(500).json({ message: 'Internal server error.' });
-};
+  return response.status(500).json({ message: 'Internal server error.' })
+}
 
-export default errorHandler;
+export default errorHandler
