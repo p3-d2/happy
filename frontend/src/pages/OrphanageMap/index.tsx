5df22b7b28b2fcfacdbc6c708a14efd6b55d42ac
Pedro-frontEnd <pedro.contato.email@gmail.com>
2020-10-20 13:02:46 -0300

    maps page animations

--- frontend/src/pages/OrphanageMap/index.tsx
+++ frontend/src/pages/OrphanageMap/index.tsx
@@ -7,13 +7,15 @@ import {
   Container,
   Aside,
   Header,
-  Imagem,
+  Img,
   Title,
   Details,
   Footer,
   Strong,
   Span,
+  MapContainer,
   Link,
+  IconWrapper,
   PlusIcon,
   ArrowIcon
 } from './styles'
@@ -36,51 +38,142 @@ const OrphanageMap: React.FC = () => {
     api.get('orphanages').then(response => setOrphanages(response.data))
   }, [])
 
+  const aside = {
+    hidden: { x: -400 },
+    visible: {
+      x: 0,
+      transition: { delay: 0.3, duration: 0.8, type: 'spring' }
+    }
+  }
+
+  const logo = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { delay: 1.4, duration: 0.5 }
+    }
+  }
+
+  const title = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { delay: 1.7, duration: 0.5 }
+    }
+  }
+
+  const details = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { delay: 2.0, duration: 0.5 }
+    }
+  }
+
+  const strong = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { delay: 2.3, duration: 0.5 }
+    }
+  }
+
+  const span = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { delay: 2.6, duration: 0.5 }
+    }
+  }
+
+  const map = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { delay: 3.1, duration: 1 }
+    }
+  }
+
+  const button = {
+    hidden: { opacity: 0, scale: 1.4 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: { delay: 4.3, duration: 1, type: 'spring' }
+    },
+    hover: {
+      backgroundColor: colors.secondary,
+      transition: { duration: 0.3 }
+    }
+  }
+
   return (
     <Container>
-      <Aside>
+      <Aside variants={aside} initial="hidden" animate="visible">
         <Header>
-          <Imagem src={mapMarker} alt="Happy" />
+          <Img
+            src={mapMarker}
+            alt="Happy"
+            variants={logo}
+            initial="hidden"
+            animate="visible"
+          />
 
-          <Title>Escolha um orfanato no mapa</Title>
-          <Details>Muitas crianças estão esperando a sua visita</Details>
+          <Title variants={title} initial="hidden" animate="visible">
+            Escolha um orfanato no mapa
+          </Title>
+          <Details variants={details} initial="hidden" animate="visible">
+            Muitas crianças estão esperando a sua visita
+          </Details>
         </Header>
 
         <Footer>
-          <Strong>Rio de Janeiro</Strong>
-          <Span>Rio de Janeiro</Span>
+          <Strong variants={strong} initial="hidden" animate="visible">
+            Rio de Janeiro
+          </Strong>
+          <Span variants={span} initial="hidden" animate="visible">
+            Rio de Janeiro
+          </Span>
         </Footer>
       </Aside>
 
-      <Map
-        center={[-22.8965395, -43.6279325]}
-        zoom={15}
-        style={{ width: '100%', height: '100%' }}
-      >
-        <TileLayer
-          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-          /* don't copy my credential */
-        />
+      <MapContainer variants={map} initial="hidden" animate="visible">
+        <Map
+          center={[-22.8965395, -43.6279325]}
+          zoom={15}
+          style={{ width: '100%', height: '100%' }}
+        >
+          <TileLayer
+            url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+          />
 
-        {orphanages.map(({ id, latitude, longitude, name }) => (
-          <Marker key={id} icon={mapIcon} position={[latitude, longitude]}>
-            <Popup
-              closeButton={false}
-              minWidth={240}
-              maxWidth={240}
-              className="map-popup"
-            >
-              {name}
-              <Link to={`/orphanages/${id}`}>
-                <ArrowIcon size={20} color={colors.white} />
-              </Link>
-            </Popup>
-          </Marker>
-        ))}
-      </Map>
+          {orphanages.map(({ id, latitude, longitude, name }) => (
+            <Marker key={id} icon={mapIcon} position={[latitude, longitude]}>
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className="map-popup"
+              >
+                {name}
+                <Link to={`/orphanages/${id}`}>
+                  <ArrowIcon size={20} color={colors.white} />
+                </Link>
+              </Popup>
+            </Marker>
+          ))}
+        </Map>
+      </MapContainer>
 
       <Link to="/orphanages/create">
-        <PlusIcon size={32} color={colors.white} />
+        <IconWrapper
+          variants={button}
+          initial="hidden"
+          animate="visible"
+          whileHover="hover"
+        >
+          <PlusIcon size={32} color={colors.white} />
+        </IconWrapper>
       </Link>
     </Container>
   )
--- frontend/src/pages/OrphanageMap/styles.ts
+++ frontend/src/pages/OrphanageMap/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 import { Link as ReactRouterDomLink } from 'react-router-dom'
 import { FiPlus, FiArrowRight } from '../../styles/icons'
 
@@ -25,7 +26,7 @@ export const Container = styled.div`
       position: relative;
 
       color: ${props => props.theme.colors.colorPopup};
-      font-size: ${props => props.theme.fontSizes.normal};
+      font-size: ${props => props.theme.fontSizes.small};
       font-weight: ${props => props.theme.fontWeights.bold};
       margin: 8px 12px;
 
@@ -58,7 +59,80 @@ export const Container = styled.div`
   }
 `
 
-export const Aside = styled.aside`
+export const PopupWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  background: ${props => props.theme.colors.shadow};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const PopupContainer = styled.div`
+  position: relative;
+  padding: 40px;
+
+  width: 400px;
+  height: 300px;
+  border-radius: 20px;
+
+  background: ${props => props.theme.colors.white};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const PopupTop = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const PopupImg = styled.img`
+  width: 72px;
+  height: 72px;
+
+  margin-bottom: 12px;
+`
+
+export const PopupTitle = styled.h2`
+  color: ${props => props.theme.colors.primary};
+  font-weight: ${props => props.theme.fontWeights.extraBold};
+`
+
+export const PopupSpinner = styled.div`
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid ${props => props.theme.colors.background};
+  border-top-color: ${props => props.theme.colors.primary};
+  animation: spinner 0.6s linear infinite;
+`
+
+export const PopupTip = styled.div`
+  position: absolute;
+  left: 50%;
+  bottom: -25px;
+  transform: translateX(-50%);
+
+  width: 0;
+  height: 0;
+  border-left: 25px solid transparent;
+  border-right: 25px solid transparent;
+  border-top: 25px solid ${props => props.theme.colors.white};
+`
+
+export const Aside = styled(motion.aside)`
   width: 400px;
   background: ${props =>
     `linear-gradient(329.54deg, ${props.theme.colors.primary}, ${props.theme.colors.secondary})`};
@@ -72,16 +146,16 @@ export const Aside = styled.aside`
 
 export const Header = styled.header``
 
-export const Imagem = styled.img``
+export const Img = styled(motion.img)``
 
-export const Title = styled.h2`
+export const Title = styled(motion.h2)`
   font-size: ${props => props.theme.fontSizes.big};
   font-weight: ${props => props.theme.fontWeights.bold};
   line-height: ${props => props.theme.lineHeights.big};
   margin-top: 32px;
 `
 
-export const Details = styled.p`
+export const Details = styled(motion.p)`
   line-height: ${props => props.theme.lineHeights.small};
   margin-top: 24px;
 `
@@ -93,11 +167,16 @@ export const Footer = styled.footer`
   line-height: ${props => props.theme.lineHeights.small};
 `
 
-export const Strong = styled.strong`
+export const Strong = styled(motion.strong)`
   font-weight: ${props => props.theme.fontWeights.bold};
 `
 
-export const Span = styled.span``
+export const Span = styled(motion.span)``
+
+export const MapContainer = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+`
 
 export const Link = styled(ReactRouterDomLink)`
   z-index: 10;
@@ -105,21 +184,17 @@ export const Link = styled(ReactRouterDomLink)`
   position: absolute;
   right: 40px;
   bottom: 40px;
+`
 
-  width: 64px;
-  height: 64px;
-  background: ${props => props.theme.colors.quintenary};
-  border-radius: 20px;
+export const IconWrapper = styled(motion.div)`
+  width: 80px;
+  height: 80px;
+  background: ${props => props.theme.colors.primary};
+  border-radius: 30px;
 
   display: flex;
   align-items: center;
   justify-content: center;
-
-  transition: background-color 500ms;
-
-  &:hover {
-    background: ${props => props.theme.colors.sextiary};
-  }
 `
 
 export const PlusIcon = styled(FiPlus)``
